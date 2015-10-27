bff13cf477225789e9855ffab113afaaa9a20dcc
ChrisOrTwo <krzysztof.krzyskow@gmail.com>
2015-10-27 21:49:16 +0100

    adding CORS headers to nancyfx api

--- Src/Outgo.Service/Bootstrap/ServiceBootstraper.cs
+++ Src/Outgo.Service/Bootstrap/ServiceBootstraper.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using Nancy;
+using Nancy.Bootstrapper;
 using Nancy.TinyIoc;
 using Outgo.Service.Data;
 using Outgo.Service.Service.Modules;
@@ -14,6 +15,18 @@ namespace Outgo.Service.Bootstrap
 			{
 				ConnectionString = "Server=127.0.0.1;Port=5432;Database=outgo;Uid=postgres;Pwd=postgres;"
 			};
+		}
+
+		protected override void RequestStartup(TinyIoCContainer container, IPipelines pipelines, NancyContext context)
+		{
+			//CORS Enable
+			pipelines.AfterRequest.AddItemToEndOfPipeline(ctx =>
+			{
+				ctx.Response
+					.WithHeader("Access-Control-Allow-Origin", "*")
+					.WithHeader("Access-Control-Allow-Methods", "POST,GET")
+					.WithHeader("Access-Control-Allow-Headers", "Accept, Origin, Content-type");
+			});
 		}
 
 		protected override void ConfigureApplicationContainer(TinyIoCContainer container)
--- Src/Outgo.UI/App/Controllers/PaymentsController.js
+++ Src/Outgo.UI/App/Controllers/PaymentsController.js
@@ -1,9 +1,15 @@
 ﻿var app = angular.module('app');
 
-app.controller('PaymentsController', function ($scope) {
+app.controller('PaymentsController', function ($scope, $http) {
 
-    $scope.Payments = [
-        { name: "John", snippet: "snippet" },
-        { name: "John", snippet: "snippet" }];
+    $scope.message = "This is payments controller";
+
+    $http
+        .get("http://localhost:8888/nancy/payments")
+        .success(function (data) {
+            alert(data);
+            $scope.message = "it's alive!!!!";
+            $scope.data = data;
+        });
 
 });
